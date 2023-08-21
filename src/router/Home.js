d19441abae7e86b88a5029ed1a6d4a5fce6a03ff
Langgeng Kanugrahan <lkanugrahan@gmail.com>
2023-08-21 20:11:39 +0700

    update home detail

--- src/router/Home.js
+++ src/router/Home.js
@@ -1,7 +1,8 @@
 const app = require("express");
 const router = app.Router();
-const { getDataSearch } = require("../controller/HomeController");
+const { getDataSearch, getDataHiring } = require("../controller/HomeController");
 
 router.get("/", getDataSearch)
+router.get("/:id", getDataHiring)
 
 module.exports = router;
